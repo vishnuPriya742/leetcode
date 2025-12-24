952cfe0ae184370c60c5e7c0c414f2d1808c2c80
Aravelli Vishnu Priya <23A91A05E1@aec.edu.in>
2025-12-24 15:01:38 +0530

    Time: 88 ms (28.46%) | Memory: 56.8 MB (18.80%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<s.size();i++){
+            if(freq[s[i]-'a']<k){
+                int left = longestSubstring(s.substr(0,i),k);
+                int right = longestSubstring(s.substr(i+1),k);
+                return max(left,right);
+            }
+        }
+       return s.size();
+    }
+};
