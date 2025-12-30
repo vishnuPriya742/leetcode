c63556bd0b46a41f12b6e9ed3f7a574a07a5e95f
Aravelli Vishnu Priya <23A91A05E1@aec.edu.in>
2025-12-30 16:04:43 +0530

    Time: 8 ms (61.01%) | Memory: 71.4 MB (71.03%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-int fun(vector<int>&nums,int k){
-      int i = 0;
-        int cnt = 0;
-        int ans = 0;
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int i = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int ans1 = 0;
+        int ans2 = 0;
       for(int j=0;j<nums.size();j++){
-            cnt+=nums[j]%2;
-            while(cnt>k){
-                cnt-=nums[i]%2;
+            cnt1+=nums[j]%2;
+            while(cnt1>k){
+                cnt1-=nums[i]%2;
                 i++;
             }
-            ans+=j-i+1;
+            ans1+=j-i+1;
       }
-      return ans;
-}
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        return fun(nums,k)-fun(nums,k-1);
+      i=0;
+      k=k-1;
+          for(int j=0;j<nums.size();j++){
+            cnt2+=nums[j]%2;
+            while(cnt2>k){
+                cnt2-=nums[i]%2;
+                i++;
+            }
+            ans2+=j-i+1;
+      }
+      return ans1-ans2;
     }
 };
